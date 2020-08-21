3f5d13f0347b04cf17739fba3a0512961fdfea38
asxisto <asxisto@gmail.com>
2020-08-21 23:07:37 +0100

    search input with current date and time

--- client/src/views/HomeView.jsx
+++ client/src/views/HomeView.jsx
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 
 class HomeView extends Component {
-  handleCurrentTime() {
-    var d = new Date().toGMTString(),
-      h = d.getHours(),
-      m = d.getMinutes();
+  constructor(props) {
+    super(props);
+    this.state = {
+      day: '2020-08-27',
+      time: '14:00'
+    };
+  }
+
+  componentDidMount() {
+    const currentDate = new Date();
+
+    let day = currentDate.getDate();
+    let month = currentDate.getMonth() + 1;
+    let year = currentDate.getFullYear();
+    if (day < 10) day = '0' + day;
+    if (month < 10) month = '0' + month;
+    const currentDay = year + '-' + month + '-' + day;
+
+    let h = currentDate.getHours();
+    let m = currentDate.getMinutes();
     if (h < 10) h = '0' + h;
     if (m < 10) m = '0' + m;
-    'input[type="time"][value="now"]'.each(function() {
-      this.attr({ value: h + ':' + m });
+    const currentTime = h + ':' + m;
+
+    this.setState({
+      day: currentDay,
+      time: currentTime
     });
   }
 
+  handleCurrentTime() {}
   render() {
     return (
       <div>
@@ -21,10 +41,10 @@ class HomeView extends Component {
         <form action="">
           <label htmlFor="city-input">City</label>
           <input type="text" name="" placeholder="Park here..." id="" />
-          <label htmlFor="city-input">Date:</label>
-          <input type="date" name="" placeholder="Park here..." id="" value="2020/08/21" onChange={this.handleCurrentTime} />
-          <label htmlFor="city-input">Starting Time:</label>
-          <input type="time" name="" placeholder="Park here..." id="" value="14:00" onChange={this.handleCurrentTime} />
+          <label htmlFor="date-input">Date:</label>
+          <input type="date" name="date-input" id="date-input" value={this.state.day} onChange={this.handleCurrentTime} onLoad={this.handleCurrentTime} />
+          <label htmlFor="time-input">Starting Time:</label>
+          <input type="time" name="time-input" id="time-input" value={this.state.time} onChange={this.handleCurrentTime} onLoad={this.handleCurrentTime} />
           <button>Search</button>
         </form>
       </div>
