880f2432fdbff5abe8ee69046f9661f494de13d1
Eloisa Seabra <seabra.eloisa@gmail.com>
2020-08-23 17:16:39 +0100

    fixed merge services parking.js

--- client/src/services/parking.js
+++ client/src/services/parking.js
@@ -9,15 +9,10 @@ export const loadParking = () => {
   api.get('/list').then(response => response.data);
 };
 
-<<<<<<< HEAD
-export const createParking = (body) => {
-  api.post("/create", body).then((response) => response.data);
-=======
-export const uploadParking = body => {
+export const createParking = body => {
   api.post('/create', body).then(response => response.data);
 };
 
 export const searchParking = body => {
   api.get('/list', body).then(response => response.data);
->>>>>>> 98559d69cf6c2d36aaa4b79a47def38326c1d1fb
 };
