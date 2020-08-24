30b2e81751d313bd6537a96f0becf1656361b244
Eloisa Seabra <seabra.eloisa@gmail.com>
2020-08-24 22:11:01 +0100

    m

--- client/src/views/profile/ProfileView.jsx
+++ client/src/views/profile/ProfileView.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { loadProfile } from '../../services/profile';
 import { deleteSingleParking } from '../../services/parking';
-import ParkingSpot from './../../components/ParkingSpot/Index';
 
 class ProfileView extends Component {
   constructor() {
@@ -15,21 +14,16 @@ class ProfileView extends Component {
   }
 
   componentDidMount() {
-    this.handleLoadProfile();
+    this.loadProfile();
   }
 
-  handleLoadProfile = () => {
+  loadProfile = () => {
     loadProfile()
       .then(data => {
-        // console.log('this comes from backend: ', data);
-        // console.log('this comes from state: ', this.props.user);
-
-        const user = this.props.user;
-        const parkings = data.parking;
-
+        const user = data.user;
         this.setState({
           user,
-          ownParkings: parkings,
+          ownParkings: user.parkings,
           loaded: true
         });
       })
@@ -39,11 +33,12 @@ class ProfileView extends Component {
   };
 
   handleParkingDeletion = index => {
-    const id = this.state.ownParkings[index]._id;
+    const id = this.state.user.parkings[index]._id;
+    // const parkingList = [...this.state.user.parkings];
 
     deleteSingleParking(id)
       .then(() => {
-        this.handleLoadProfile();
+        this.loadProfile();
       })
       .catch(error => {
         console.log(error);
@@ -53,7 +48,6 @@ class ProfileView extends Component {
   render() {
     const user = this.state.user;
     const parkings = this.state.ownParkings;
-
     return (
       <div>
         {this.state.loaded && (
@@ -72,14 +66,25 @@ class ProfileView extends Component {
             <div className="own-parkings">
               <hr />
               <h4>My spots:</h4>
-              {(this.state.ownParkings.length && (
+              {(parkings && (
                 <>
-                  {user.parkings.map((parking, index) => (
-                    <ParkingSpot
-                      key={parking._id}
-                      parking={parking}
-                      onClick={this.handleParkingDeletion(index)}
-                    />
+                  {parkings.map((parking, index) => (
+                    <div key={parking._id}>
+                      <h5>Location: {parking.location}</h5>
+                      <small>{parking.description}</small>
+                      <p>Price: {parking.price}$/hr</p>
+                      <Link to={`/parking/${parking._id}`}>Details </Link>
+                      <Link to={`/parking/${parking._id}/edit`}>
+                        {' '}
+                        Edit Parking{' '}
+                      </Link>
+                      <button onClick={() => this.handleParkingDeletion(index)}>
+                        Delete
+                      </button>
+                      {/* <form onSubmit={this.handleParkingDeletion}>
+                    <button>Delete</button>
+                  </form> */}
+                    </div>
                   ))}
                 </>
               )) || <p>You have no parking spots to rent.</p>}
@@ -92,3 +97,17 @@ class ProfileView extends Component {
 }
 
 export default ProfileView;
+
+// import React from 'react';
+
+// function ProfileView(props) {
+//   return (
+//     <div>
+//       {console.log(props.user)}
+//       <h1>Profile View</h1>
+//       <h4>name:</h4>
+//     </div>
+//   );
+// }
+
+// export default ProfileView;
