c62190e450a72118cae37a887992bdad280ef0bb
Brett Sherwood <clockmakerbrett@gmail.com>
2020-08-25 14:34:15 -0400

    added geoquery functionality

--- routes/parking.js
+++ routes/parking.js
@@ -1,38 +1,47 @@
-const express = require('express');
+const express = require("express");
 
-const Parking = require('../models/parking');
-const User = require('../models/user');
+const Parking = require("../models/parking");
+const User = require("../models/user");
 
-const routeAuthenticationGuard = require('./../middleware/route-authentication-guard');
+const routeAuthenticationGuard = require("./../middleware/route-authentication-guard");
 
-const multer = require('multer');
-const cloudinary = require('cloudinary');
-const multerStorageCloudinary = require('multer-storage-cloudinary');
+const multer = require("multer");
+const cloudinary = require("cloudinary");
+const multerStorageCloudinary = require("multer-storage-cloudinary");
 
 const parkingRouter = new express.Router();
 
 const storage = new multerStorageCloudinary.CloudinaryStorage({
-  cloudinary: cloudinary.v2
+  cloudinary: cloudinary.v2,
 });
 const upload = multer({ storage });
 
-parkingRouter.get('/list', (request, response, next) => {
-  const { coordinates } = request.body;
-
-  Parking.find()
-    .populate('user')
-    .then(spots => {
+parkingRouter.get("/list", (request, response, next) => {
+  const coordinates = request.query.coordinates;
+  const stringLng = coordinates[0];
+  const lng = Number(stringLng);
+  const stringLat = coordinates[1];
+  const lat = Number(stringLat);
+  const maxLng = lng + 0.1;
+  const minLng = lng - 0.1;
+  const maxLat = lat + 0.1;
+  const minLat = lat - 0.1;
+  Parking.find({ lng: { $gt: minLng, $lt: maxLng }, lat: { $gt: minLat, $lt: maxLat } })
+    .find({ isRented: false })
+    .populate("user")
+    .then((spots) => {
       response.json({ spots });
     })
-    .catch(error => {
+    .catch((error) => {
+      console.log(error);
       next(error);
     });
 });
 
-parkingRouter.get('/:id', async (request, response, next) => {
+parkingRouter.get("/:id", async (request, response, next) => {
   const id = request.params.id;
   try {
-    const spot = await Parking.findById(id).populate('user');
+    const spot = await Parking.findById(id).populate("user");
     if (spot) {
       response.json({ spot });
     } else {
@@ -43,7 +52,7 @@ parkingRouter.get('/:id', async (request, response, next) => {
   }
 });
 
-parkingRouter.post('/create', upload.single('photo'), (req, res, next) => {
+parkingRouter.post("/create", upload.single("photo"), (req, res, next) => {
   let url;
   if (req.file) {
     url = req.file.path;
@@ -58,59 +67,50 @@ parkingRouter.post('/create', upload.single('photo'), (req, res, next) => {
     lat: numLat,
     price,
     user: req.user._id,
-    photo: url
+    photo: url,
   })
-    .then(document => {
+    .then((document) => {
       res.json({ document });
     })
-    .catch(error => {
+    .catch((error) => {
       console.log(error);
       next(error);
     });
 });
 
-parkingRouter.delete(
-  '/:id',
-  routeAuthenticationGuard,
-  async (request, response, next) => {
-    const id = request.params.id;
+parkingRouter.delete("/:id", routeAuthenticationGuard, async (request, response, next) => {
+  const id = request.params.id;
 
-    Parking.findOneAndDelete({ _id: id, user: request.user._id })
-      .then(() => {
-        response.json({});
-      })
-      .catch(error => {
-        console.log(error);
-        next(error);
-      });
+  Parking.findOneAndDelete({ _id: id, user: request.user._id })
+    .then(() => {
+      response.json({});
+    })
+    .catch((error) => {
+      console.log(error);
+      next(error);
+    });
+});
+
+parkingRouter.patch("/:id", routeAuthenticationGuard, upload.single("photo"), (request, response, next) => {
+  const id = request.params.id;
+  const { location, description, lat, lng, price } = request.body;
+
+  let data;
+  let url;
+  if (request.file) {
+    url = request.file.path;
+    data = { location, description, lat, lng, price, url };
+  } else {
+    data = { location, description, lat, lng, price };
   }
-);
 
-parkingRouter.patch(
-  '/:id',
-  routeAuthenticationGuard,
-  upload.single('photo'),
-  (request, response, next) => {
-    const id = request.params.id;
-    const { location, description, lat, lng, price } = request.body;
-
-    let data;
-    let url;
-    if (request.file) {
-      url = request.file.path;
-      data = { location, description, lat, lng, price, url };
-    } else {
-      data = { location, description, lat, lng, price };
-    }
-
-    Parking.findByIdAndUpdate(id, data)
-      .then(spot => {
-        response.json({ spot });
-      })
-      .catch(error => {
-        next(error);
-      });
-  }
-);
+  Parking.findByIdAndUpdate(id, data)
+    .then((spot) => {
+      response.json({ spot });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
 
 module.exports = parkingRouter;
